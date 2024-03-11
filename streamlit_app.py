4a6059d70650c870c3e581aff7d6b46d661f127e
Octopanpan <162982942+Octopanpan@users.noreply.github.com>
2024-03-11 12:35:10 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ st.write("je veux faire mon blog sur l'album viva la vida helllp ToT")
 name=st.text_input("Your name")
 st.write("Hello"+ name)
 genre=st.radio("what's your favorite music genre ?",
-               [":rainbow(Pop)]", "***Rock***", "jazz :saxophone":],
+               [":rainbow(Pop)]", "***Rock***", "jazz :saxophone"],
                caption = ["Dance and have fun.", "look at that guitar rift !","Chill and relax."])
 if genre == ':***Rock***':
   st.write('you selected Rock.')
