4332c13a063d1eea367cc3d270b3ec1af1e18ade
Octopanpan <162982942+Octopanpan@users.noreply.github.com>
2024-03-11 12:37:06 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ st.write("Hello")
 st.write("je veux faire mon blog sur l'album viva la vida helllp ToT")
 name=st.text_input("Your name")
 st.write("Hello"+ name)
-genre=st.radio(
+genre = st.radio(
   "what's your favorite music genre ?",
   [":rainbow(Pop)]", "***Rock***", "jazz :saxophone"],
   caption = ["Dance and have fun.", "look at that guitar rift !","Chill and relax."])
