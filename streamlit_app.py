98528fdc8861b94b02fc8bb340fd663778665c52
Octopanpan <162982942+Octopanpan@users.noreply.github.com>
2024-03-11 12:52:10 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st 
 st.write("Hello")
 st.write("je veux faire mon blog sur l'album viva la vida helllp ToT")
+st.write("je prefere quznd on trvaille sur le pc meme si c'est drole ce qu'on fait mtn avec la liste mdr")
 name=st.text_input("Your name")
 st.write("Hello"+ name)
 
