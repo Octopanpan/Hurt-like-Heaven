4209737b548e1266287a5a61792cb664af4651ca
Octopanpan <162982942+Octopanpan@users.noreply.github.com>
2024-03-11 12:38:19 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,17 @@ st.write("Hello")
 st.write("je veux faire mon blog sur l'album viva la vida helllp ToT")
 name=st.text_input("Your name")
 st.write("Hello"+ name)
+import streamlit as st
+
+genre = st.radio(
+    "What's your favorite movie genre",
+    [":rainbow[Comedy]", "***Drama***", "Documentary :movie_camera:"],
+    captions = ["Laugh out loud.", "Get the popcorn.", "Never stop learning."])
+
+if genre == ':rainbow[Comedy]':
+    st.write('You selected comedy.')
+else:
+    st.write("You didn\'t select comedy.")
 genre = st.radio(
   "what's your favorite music genre ?",
   [":rainbow(Pop)]", "***Rock***", "jazz :saxophone"],
