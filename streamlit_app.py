d6f1beeb35784c3d1d9b8c7847b084c0cda87ac3
Octopanpan <162982942+Octopanpan@users.noreply.github.com>
2024-03-11 12:34:45 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,15 @@
 import streamlit as st 
-st.write("mello")
+st.write("Hello")
 st.write("je veux faire mon blog sur l'album viva la vida helllp ToT")
 name=st.text_input("Your name")
 st.write("Hello"+ name)
+genre=st.radio("what's your favorite music genre ?",
+               [":rainbow(Pop)]", "***Rock***", "jazz :saxophone":],
+               caption = ["Dance and have fun.", "look at that guitar rift !","Chill and relax."])
+if genre == ':***Rock***':
+  st.write('you selected Rock.')
+else:
+  st.write("you didn\'t select Rock.")
+               
+
+
