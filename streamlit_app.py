50522a48c4601d4c571e9839170f5857ae9cab07
Octopanpan <162982942+Octopanpan@users.noreply.github.com>
2024-03-18 11:43:46 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,29 +1,3 @@
-import streamlit as st 
-st.write("Hello")
-st.write("je veux faire mon blog sur l'album viva la vida helllp ToT")
-st.write("je prefere quznd on trvaille sur le pc meme si c'est drole ce qu'on fait mtn avec la liste mdr")
-name=st.text_input("Your name")
-st.write("Hello"+ name)
-
-import streamlit as st
-
-genre = st.radio(
-    "What's your favorite music genre",
-    [":rainbow[Pop]", "***Rock***", "Jazz :saxophone:"],
-    captions = ["Dancing and Singing.", "Look at that sick guitzr riff!", "Chill'out and relax."])
-
-if genre == ':rainbow[Pop]':
-    st.write('You selected pop.')
-else:
-    st.write("You didn\'t select pop.")
-
-input = "lapin"
-list_possibilities=["rabbit","race","rhyme","rogue"]
-correct_answer = "rabbit"
-st.write("Traduis: lapin")
-for i in range(len(list_possibilities)):
-    st.button(list_possibilities[i]
-
 import streamlit as st
 import pandas as pd
 voc=pd.read_cvs('https://docs.google.com/spreadsheets/d/e/2PACX-1vRQ2cabWZZfzicRWSCzVA6rZKOWdNfqpy5kqxrLn6V4VThfaA0bIqd4k_PZv3z5CJPpb18PFhyv8-Ee/pubhtml')
