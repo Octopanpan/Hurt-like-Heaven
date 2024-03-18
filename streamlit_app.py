7e168fb4fb2be50a5c5dae898c7ff9cdd51a9bc4
Octopanpan <162982942+Octopanpan@users.noreply.github.com>
2024-03-18 11:58:03 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-voc=pd.read_cvs('https://docs.google.com/spreadsheets/d/e/2PACX-1vRQ2cabWZZfzicRWSCzVA6rZKOWdNfqpy5kqxrLn6V4VThfaA0bIqd4k_PZv3z5CJPpb18PFhyv8-Ee/pub?output=csv')
+import numpy as np
+voc=pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vRQ2cabWZZfzicRWSCzVA6rZKOWdNfqpy5kqxrLn6V4VThfaA0bIqd4k_PZv3z5CJPpb18PFhyv8-Ee/pub?output=csv')
 st.dataframe(voc)
-              
-    
