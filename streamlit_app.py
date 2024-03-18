fc9106eda2962be8e43415e6118d6772a4cd5979
Octopanpan <162982942+Octopanpan@users.noreply.github.com>
2024-03-18 12:02:25 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import numpy as np
 voc=pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vRQ2cabWZZfzicRWSCzVA6rZKOWdNfqpy5kqxrLn6V4VThfaA0bIqd4k_PZv3z5CJPpb18PFhyv8-Ee/pub?output=csv')
 st.dataframe(voc)
 l=voc.shape[0]
-i=hp.ra.ndom.choice(range(l)) 
+i=np.ra.ndom.choice(range(l)) 
 word_fr= voc['Definiton'].values[i] 
 word_chi= voc['Hanzi'].values[i] 
 st.write(word_frt+" "+ word_chi)
