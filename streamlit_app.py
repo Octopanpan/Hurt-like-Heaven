2a7015494e5383ec4ad86aca3d3b4bd527f2a998
Octopanpan <162982942+Octopanpan@users.noreply.github.com>
2024-03-25 12:01:32 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,10 @@ import pandas as pd
 import numpy as np
 voc = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vSeARXO3MT92XWpg2IwyQOQ8Wi2upeEkqJvNJz5i3bRqHdJIrTchGBBclVu-3Jd1ohYKM4IxecgV64I/pub?output=csv ')
 l = voc.shape[0]
+if "indice" in st.session_state:
+  indices=st.session_state["indices"]
+else:
+  îdices=np.random
 indices = np.random.choice(l, size=4, replace=False)
 j = np.random.choice(indices)
 word_fr=voc['Définition'].values[j]
@@ -11,8 +15,11 @@ st.write('Traduis: '+word_fr)
 def is_correct(i, j):
   if i==j:
     st.write("Bien joué !")
+    si bravo:
+    del st.session_state["indices"]
   else:
     st.write("Perdu !")
+    st.session_state["indices"]=indices
 
 col1, col2 = st.columns(2) 
 with col1:
@@ -25,6 +32,5 @@ with col2:
 
 st.button("Seconday button")   #st.button default type is secondary
 st.button("Primary button", type="primary")
-st.session_state["my_var"]=1
-if "my_var" in st.session_state
-del st.session_state["my var"]
+
+
